eae939502ecd7377b95ee450da9d67d6f68774a6
alizhanserkeshev <157126646+alizhanserkeshev@users.noreply.github.com>
2024-12-10 22:13:05 +0500

    Update ResearchProject.java

--- Code/src/Research/ResearchProject.java
+++ Code/src/Research/ResearchProject.java
@@ -1,66 +1,21 @@
 package Research;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
+import java.util.Vector;
+import Users.Researcher;
+import Users.User;
+import Exceptions.ImposterException;
 
-class ResearchProject {
-    private String topic;
-    private List<ResearchPaper> publishedPapers;
-    private List<Researcher> participants;
+public class ResearchProject implements Serializable{
+	private String topic;
+	
+	Vector<ResearchPaper> papers = new Vector<>();
+	
+	public String getTopic() {
+		return topic;
+	}
 
-    public ResearchProject(String topic) {
-        this.topic = topic;
-        this.publishedPapers = new ArrayList<>();
-        this.participants = new ArrayList<>();
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public List<ResearchPaper> getPublishedPapers() {
-        return publishedPapers;
-    }
-
-    public List<Researcher> getParticipants() {
-        return participants;
-    }
-
-    public void addPaper(ResearchPaper paper) {
-        this.publishedPapers.add(paper);
-    }
-
-    public void addParticipant(Employee employee) throws ImposterException {
-        if (!(employee instanceof Researcher)) {
-            throw new ImposterException("Employee is not a Researcher and cannot join the Research Project.");
-        }
-        participants.add((Researcher) employee);
-    }
-
-    public void printParticipants() {
-        System.out.println("Participants in project \"" + topic + "\":");
-        for (Researcher researcher : participants) {
-            System.out.println(researcher.getName());
-        }
-    }
-
-    public void printPapers() {
-        System.out.println("Published papers for project \"" + topic + "\":");
-        for (ResearchPaper paper : publishedPapers) {
-            System.out.println(paper);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "ResearchProject{" +
-               "topic='" + topic + '\'' +
-               ", publishedPapers=" + publishedPapers +
-               ", participants=" + participants +
-               '}';
-    }
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
 }
